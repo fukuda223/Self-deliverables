566c06a7c8e348575600dfb5be808a39c5cd1c73
fukuda223 <168424357+fukuda223@users.noreply.github.com>
2024-06-12 17:52:05 +0900

    減塩・低糖な食事を継続させる食生活改善サービス
    制作途中
    起動後の画面の大まかな動作を作成

---
+++ health/health/healthApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthApp.swift
+//  health
+//
+//  Created by 福田恭太郎 on 2024/06/08.
+//
+
+import SwiftUI
+
+@main
+struct healthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
